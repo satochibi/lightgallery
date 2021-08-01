257ddb5d06c6f6c05e88bf1a701133d4c16a4b6e
satochibi <satochibi0120@icloud.com>
2021-08-01 11:47:37 +0900

    first commit

---
+++ js/setting.js
@@ -0,0 +1,27 @@
+const $lightGallery = document.getElementById('lightgallery');
+const lightGallery = window.lightGallery($lightGallery, {
+    dynamic: true,
+    plugins: [lgZoom, lgThumbnail],
+    addClass: 'lg-custom-thumbnails',
+    appendThumbnailsTo: '.lg-outer',
+    animateThumb: false,
+    allowMediaOverlap: true,
+    dynamicEl: [
+        {
+            src: "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/1_001.jpg",
+            thumb: "img/thumb1_001.jpg"
+        },
+        {
+            src: "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/1_002.jpg",
+            thumb: "img/thumb1_002.jpg"
+        },
+        {
+            src: "http://ehimestream.xsrv.jp/digital_archives/wp-content/themes/digitalarchives/image/najimi/1_003.jpg",
+            thumb: "img/thumb1_003.jpg"
+        }
+    ]
+});
+
+$lightGallery.addEventListener("click", () => {
+    lightGallery.openGallery(0);
+});
